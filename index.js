ecbc57a55ef12096bdc1e33d85f5e8e19e6c156f
leydalopez <122707667+leydalopez@users.noreply.github.com>
2023-01-14 20:21:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector("#yesBtn");
+
+yesBtn.addEventListener("click",function () {
+    alert("Yo también te amo, pero mi ramito de hello kitty para cuándo :(")
+});
+
+const noBtn = document.querySelector("#noBtn");
+
+noBtn.addEventListener("click",function () {
+    alert("no me importa, MI RAMO DE HELLO KITTY!!!")
+});
